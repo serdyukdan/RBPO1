56b9e3c79586ee225ed82ab1c52ebd4769d5bd24
serdyukdan <serdyukdan@gmail.com>
2023-09-22 15:37:31 +0300

    add

---
+++ RBPO1/WritePersonData.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+void WritePersonData(string& height, string& weight, const unsigned short* age, string salary)
+{
+	string tempAge();
+	cin >> height;
+	cin >> weight;
+	cin >> tempAge;
+	cin >> salary;
+}
